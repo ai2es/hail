500212a89f9f9ff85251633f0201e3d1214e220b
Tobias Schmidt <tgschmidt@schooner1.oscer.ou.edu>
2022-09-25 15:46:42 -0500

    Added new min/max script and also made preproccessor support netcdf files output

---
+++ src/find_min_max.py
@@ -0,0 +1,54 @@
+import xarray as xr
+import numpy as np
+import glob
+import os
+
+EXAMPLES_NETCDF_GLOB = "/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/examples/*"
+LABELS_NETCDF_GLOB = "/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/labels/*"
+MIN_MAX_FILES_DIR = ""
+
+examples_files = glob.glob(EXAMPLES_NETCDF_GLOB)
+examples_files.sort()
+labels_files = glob.glob(LABELS_NETCDF_GLOB)
+labels_files.sort()
+
+one_examples_ds = xr.open_dataset(examples_files[0])
+example_keys = [key for key in one_examples_ds.keys()]
+one_examples_ds.close()
+
+one_labels_ds = xr.open_dataset(labels_files[0])
+label_keys = [key for key in one_labels_ds.keys()]
+one_labels_ds.close()
+
+examples_max = np.full(len(example_keys), -np.inf)
+labels_max = np.full(len(label_keys), -np.inf)
+examples_min = np.full(len(example_keys), np.inf)
+labels_min = np.full(len(label_keys), np.inf)
+
+for examples_file, labels_file in zip(examples_files, labels_files):
+    examples_ds = xr.open_dataset(examples_file)
+    labels_ds = xr.open_dataset(labels_file)
+
+    examples_array = examples_ds.to_array().to_numpy()
+    labels_array = labels_ds.to_array().to_numpy()
+
+    examples_ds.close()
+    labels_ds.close()
+
+    for i in range(len(examples_max)):
+        examples_max[i] = np.max((examples_max[i], np.max(examples_array[i,...])))
+        examples_min[i] = np.min((examples_min[i], np.min(examples_array[i,...])))
+
+    for i in range(len(labels_max)):
+        labels_max[i] = np.max((labels_max[i], np.max(labels_array[i,...])))
+        labels_min[i] = np.min((labels_min[i], np.min(labels_array[i,...])))
+
+examples_max_ds = xr.Dataset(data_vars=dict(zip(example_keys, examples_max)))
+examples_min_ds = xr.Dataset(data_vars=dict(zip(example_keys, examples_min)))
+labels_max_ds = xr.Dataset(data_vars=dict(zip(label_keys, labels_max)))
+labels_min_ds = xr.Dataset(data_vars=dict(zip(label_keys, labels_min)))
+
+examples_max_ds.to_netcdf(os.path.join(MIN_MAX_FILES_DIR, "examples_max.nc"))
+examples_min_ds.to_netcdf(os.path.join(MIN_MAX_FILES_DIR, "examples_min.nc"))
+labels_max_ds.to_netcdf(os.path.join(MIN_MAX_FILES_DIR, "labels_max.nc"))
+labels_min_ds.to_netcdf(os.path.join(MIN_MAX_FILES_DIR, "labels_min.nc"))
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -16,14 +16,28 @@ def create_parser():
 
     parser.add_argument('--examples', type=str, default='/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/examples/*')
     parser.add_argument('--labels', type=str, default='/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/labels/*')
-    parser.add_argument('--tf_ds_dir', type=str, default='/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/tf_datasets')
-    parser.add_argument('--feature_vars_to_drop', type=str, nargs='+', default=['lon', 'lat'])
+    parser.add_argument('--output_ds_dir', type=str, default='/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/tf_datasets')
+    parser.add_argument('--min_max_dir', type=str, default='/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/patches/train_val/tf_datasets')
+    parser.add_argument('--feature_vars_to_drop', type=str, nargs='+', default=['lon', 'lat', 'hailcast'])
     parser.add_argument('--label_vars_to_drop', type=str, nargs='+', default=['time', 'lon', 'lat', 'MESH95', 'MESH_class_bin_0'])
     parser.add_argument('--approx_file_clumping_num', type=int, default=8)
     parser.add_argument('--n_parallel_runs', type=int, default=15)
     parser.add_argument('--run_num', type=int, default=0)
+    parser.add_argument('--save_as_netcdf', '-n', action='store_true')
 
     return parser
+
+
+def min_max_norm(variable, var_min, var_max):
+    """
+    This does min_max scaling on the
+    given variable.
+    """
+    if (var_max - var_min) == 0:
+        return np.zeros(variable.shape)
+    
+    variable = (variable - var_min) / (var_max - var_min)
+    return variable
 
 
 if __name__ == "__main__":
@@ -34,17 +48,26 @@ if __name__ == "__main__":
 
     netcdf_examples_dir = args["examples"]
     netcdf_labels_dir = args["labels"]
-    tf_ds_dir = args["tf_ds_dir"]
+    output_ds_dir = args["output_ds_dir"]
+    min_max_dir = args["min_max_dir"]
     input_vars_to_drop = args["feature_vars_to_drop"]
     output_vars_to_drop = args["label_vars_to_drop"]
     approx_file_clumping_num = args["approx_file_clumping_num"]
     n_parallel_runs = args["n_parallel_runs"]
     run_num = args["run_num"]
+    save_as_netcdf = args["save_as_netcdf"]
 
     input_files = glob.glob(netcdf_examples_dir)
     input_files.sort()
     output_files = glob.glob(netcdf_labels_dir)
     output_files.sort()
+
+    one_input_ds = xr.open_dataset(input_files[0])
+    input_keys = list(one_input_ds.keys())
+    one_input_ds.close()
+    one_output_ds = xr.open_dataset(output_files[0])
+    output_keys = list(one_output_ds.keys())
+    one_output_ds.close()
 
     input_files = np.array_split(np.array(input_files), len(input_files)//approx_file_clumping_num)
     output_files = np.array_split(np.array(output_files), len(output_files)//approx_file_clumping_num)
@@ -71,55 +94,55 @@ if __name__ == "__main__":
             input_ds_old.close()
             output_ds_old.close()
 
+        example_maxs = xr.open_dataset(os.path.join(min_max_dir, "examples_max.nc"))
+        example_mins = xr.open_dataset(os.path.join(min_max_dir, "examples_min.nc"))
+        label_maxs = xr.open_dataset(os.path.join(min_max_dir, "labels_max.nc"))
+        label_mins = xr.open_dataset(os.path.join(min_max_dir, "labels_min.nc"))
         output_ds = output_ds.drop(output_vars_to_drop)
+        label_maxs = label_maxs.drop(output_vars_to_drop)
+        label_mins = label_mins.drop(output_vars_to_drop)
         input_ds = input_ds.drop(input_vars_to_drop)
+        example_maxs = example_maxs.drop(input_vars_to_drop)
+        example_mins = example_mins.drop(input_vars_to_drop)
 
-        output_array = output_ds.to_array()
-        input_array = input_ds.to_array()
-        output_ds.close()
-        input_ds.close()
+        for key in input_keys:
+            input_ds[key] = min_max_norm(input_ds[key], example_mins[key], example_maxs[key])
+        # for key in output_keys:
+        #     output_ds[key] = min_max_norm(output_ds[key], label_mins[key], label_maxs[key])
 
-        output_array = output_array.astype("float32") # TODO: Make this a setting later?
-        input_array = input_array.astype("float32")
+        if save_as_netcdf:
+            input_ds_name = str(run_num) + "-" + str(i) + "_examples.nc"
+            output_ds_name = str(run_num) + "-" + str(i) + "_labels.nc"
+            input_ds.to_netcdf(os.path.join(output_ds_dir, input_ds_name))
+            output_ds.to_netcdf(os.path.join(output_ds_dir, output_ds_name))
+            output_ds.close()
+            input_ds.close()
 
-        input_array = input_array.transpose("n_samples", ...)
-        input_array = input_array.transpose(..., "variable")
+        else:
+            output_array = output_ds.to_array()
+            input_array = input_ds.to_array()
+            output_ds.close()
+            input_ds.close()
 
-        output_array = output_array.transpose("n_samples", ...)
-        output_array = output_array.transpose(..., "variable")
+            output_array = output_array.astype("float32") # TODO: Make this a setting later?
+            input_array = input_array.astype("float32")
 
-        #### TEMP ##########
-        def min_max_norm(variable):
-            """
-            This does min_max scaling on the
-            given variable.
-            """
-            if (np.max(variable) - np.min(variable)) == 0:
-                return np.zeros(variable.shape)
-            
-            variable = (variable - np.min(variable)) / (np.max(variable) - np.min(variable))
-            return variable
-        
-        output_array = output_array.to_numpy()
-        input_array = input_array.to_numpy()
-        print(np.max(input_array))
-        for j in range(input_array.shape[3]):
-            input_array[:,:,:,j] = min_max_norm(input_array[:,:,:,j])
-        # input_array = np.apply_along_axis(min_max_norm, 3, input_array)
-        print(np.max(input_array))
-        print(input_array.shape)
-        #######################
+            input_array = input_array.transpose("n_samples", ...)
+            input_array = input_array.transpose(..., "variable")
 
-        tf_ds = tf.data.Dataset.from_tensor_slices((input_array, output_array))
+            output_array = output_array.transpose("n_samples", ...)
+            output_array = output_array.transpose(..., "variable")
 
-        # input_array.close()
-        # output_array.close()
+            tf_ds = tf.data.Dataset.from_tensor_slices((input_array, output_array))
 
-        # NOTE: The save call below causes massive memory leaks. That is why we have to run this script in short bursts.
-        # see https://github.com/tensorflow/tensorflow/issues/56177
-        # and https://stackoverflow.com/questions/72308983/memory-leak-with-tensorflow-experimental-save
-        tf_ds_name = str(run_num) + "-" + str(i) + ".tf"
-        tf.data.experimental.save(tf_ds, os.path.join(tf_ds_dir, tf_ds_name))
+            input_array.close()
+            output_array.close()
+
+            # NOTE: The save call below causes massive memory leaks. That is why we have to run this script in short bursts.
+            # see https://github.com/tensorflow/tensorflow/issues/56177
+            # and https://stackoverflow.com/questions/72308983/memory-leak-with-tensorflow-experimental-save
+            tf_ds_name = str(run_num) + "-" + str(i) + ".tf"
+            tf.data.experimental.save(tf_ds, os.path.join(output_ds_dir, tf_ds_name))
 
         end_time = time.time()
-        print("Done tf dataset file number " + str(run_num) + "-" + str(i) + ". Took " + str(end_time-start_time) + " seconds to complete.")
+        print("Done dataset file number " + str(run_num) + "-" + str(i) + ". Took " + str(end_time-start_time) + " seconds to complete.")
